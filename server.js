04172cd9f81d8b6520704fc441861fc975936348
rahulp772 <rahulprajapati837@gmail.com>
2020-08-19 23:54:27 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const nodemon = require("nodemon");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./app");
+const AppError = require("./utils/appError");
 
 dotenv.config({ path: "./config.env" });
 
 // VARIABLE DECLARATIONS
 const DB = process.env.LOCAL_DATABASE;
 const port = process.env.PORT;
+
 
 const db = mongoose
   .connect(DB, {
@@ -19,6 +19,9 @@ const db = mongoose
   })
   .then(() => {
     console.log("DB Connected and Ready!");
+  }).catch(err => {
+    console.log("Something went wrong with database connection! Please see detailed Error message: ", err.message);
+    return new AppError("Something went wrong with database connection! Please see detailed Error message: ", err.message, 500);
   });
 
 const server = app.listen(port, () => {
